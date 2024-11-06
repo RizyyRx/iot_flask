8a29994821a92603896e9eef5e435a32fdc30ad3
Rizwanudeen K <rizwankendo@gmail.com>
2024-11-06 13:28:05 +0000

    did some updates for the application to work on deployment server

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append('/home/rizwankendo/flask') # this will help to find the app.py for deployment server 
+
 from flask import Flask, redirect, url_for, request, render_template, session
 import os
 import math
@@ -5,9 +8,12 @@ from src import get_config
 from src.User import User
 
 basename = get_config("basename") #use a base dir if needed and add it to app route
-# by using static_folder='assets', static_url_path=basename, now all the contents inside assets will be available in the basename iot/ in website.
-# the assets dir is kept static
-app = Flask(__name__, static_folder='assets', static_url_path=basename) #app is an object being created of class Flask  
+'''
+ * by using static_folder='assets', static_url_path=basename, now all the contents inside assets will be available in the basename iot/ in website.
+ * the assets dir is kept static
+ * put application, coz wsgi looks for var named application by default that has the flask object
+'''
+application = app = Flask(__name__, static_folder='assets', static_url_path=basename) #app is an object being created of class Flask  
 app.secret_key = get_config("secret_key") # flask uses this key to auth session
 
 @app.route(basename+'/dashboard')
